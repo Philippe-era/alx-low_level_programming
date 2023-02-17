f5f1e51b7b16b1362a374aa5bc3fd76821aa218a
Philippe-era <jeanphilippe733@gmail.com>
2023-02-17 04:49:43 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,19 @@ int main(void)
 {
 	char c;
 
+	char d;
+
 	c = 'a';
+	d = 'A';
 	while
 		(c <= 'z') {
-			if ((c != 'q' && c != 'e') && c <= 'Z')
-				putchar(c);
+			putchar(c);
 			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(c);
+			d++;
 		}
 	putchar('\n');
 	return (0);
