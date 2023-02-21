cfc8b7567103d84077629dbd9087671d11b419b2
Philippe-era <jeanphilippe733@gmail.com>
2023-02-21 15:43:54 -0800

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,27 +2,23 @@
 
 /**
  * main - main block
- * Description: computes and prints the sum of all multiples of 3 or 5
- * below 1024 ( excluded )
+ * Description: prints even number < 4000000
+ * 5 below 1024 exluded
  * Return: 0
  */
 int main(void)
 {
-	int i = 0;
-	long int a = 1, b = 2, next;
+        int a = 0, b = 1, next = 0;
+        int sum = 0;
 
-while (i < 50)
+while (next < 4000000)
 {
-	next = a + b;
-	a = b;
-	b = next;
-	printf("%lu", next);
-	if (i < 49)
-	{
-		printf(",");
-	}
-	i++;
+        next = a + b;
+        a = b;
+        b = next;
+        if (next % 2 == 0)
+                sum += next;
 }
-putchar('\n');
+printf("%d\n", sum);
 return (0);
 }
