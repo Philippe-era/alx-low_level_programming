9bb4e9645b8b0624a8b1d23325e2a8e0ad216deb
Philippe-era <jeanphilippe733@gmail.com>
2023-02-27 02:18:52 -0800

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdlib.h>
 #include <time.h>
 
@@ -20,13 +21,13 @@ for (i = 0; i < 100; i++)
 {
 pass[i] = rand() % 78;
 sum += (pass[i] + '0');
-putchar(pass[i] + '0');
+_putchar(pass[i] + '0');
 
 if ((2772 - sum) -'0' < 78)
 {
 n = 2772 - sum - '0';
 sum += n;
-putchar(n + '0');
+_putchar(n + '0');
 break;
 }
 }
