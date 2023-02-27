32c56555811a1d3a236bcf8c07bce8082ff660db
Philippe-era <jeanphilippe733@gmail.com>
2023-02-27 00:19:47 -0800

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "sluggy.h"
+
+/**
+ * puts_half - prints a string the a new line
+ * @str: pointer to the string then points
+ */
+
+void puts_half(char *str)
+{
+int len, a, i;
+
+len = 0;
+
+while (str[len] != '\0')
+{
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\0'; i++)
+{
+len++;
+}
+_putchar(str[i]);
+}
+}
+else if (len % 2)
+{
+for (n = (len - 1) / 2; n < len - 1; i++)
+{
+_putchar(str[n + 1]);
+}
+}
+
+_putchar('\n');
+}
