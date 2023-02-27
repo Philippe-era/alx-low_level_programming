d0520977318ba1ba7b338b4013960312beba6883
Philippe-era <jeanphilippe733@gmail.com>
2023-02-26 23:12:54 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "sluggy.h"
+
+/**
+ * reset_to_98 - takes integer to an int as parameter
+ * the value it should point to 98
+ * @n: points to an int
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
