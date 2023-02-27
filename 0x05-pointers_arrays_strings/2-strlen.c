84466561d8facdd81b6b0cd673d00ce0718231fc
Philippe-era <jeanphilippe733@gmail.com>
2023-02-26 23:23:25 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "sluggy.h"
+
+/**
+ * _strlen - finds the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+while (s[i])
+i++;
+
+return (i);
+
+}
