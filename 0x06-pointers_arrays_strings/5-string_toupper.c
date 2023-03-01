3dd2f2410602f518972c9b42e5d162b21543c798
Philippe-era <jeanphilippe733@gmail.com>
2023-03-01 01:27:52 -0800

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "sluggy.h"
+
+/**
+ * string_toupper - changes letters to uppercase
+ * @str: the string to be changed
+ * Return: A pointer to the changed string
+ */
+char *string_toupper(char *str)
+{
+int index = 0;
+
+while (str[index])
+{
+if (str[index] >= 'a' && str[index] <= 'z')
+str[index] -= 32;
+index++;
+}
+return (str);
+}
