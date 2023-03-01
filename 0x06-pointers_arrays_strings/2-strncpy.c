163f7cc2a5ce91a9a13964c5c7eb7b185adae2b6
Philippe-era <jeanphilippe733@gmail.com>
2023-03-01 00:26:39 -0800

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "sluggy.h"
+
+/**
+ * _strncat - concatenates two strings using input number
+ * @dest: The string to be joined
+ * @n : The number of bytes from src
+ * @src: The string to dest
+ * Return: A pointer to the resulting dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int index = 0, str_length = 0;
+
+while (src[index++])
+str_length++;
+
+for (index = 0; src[index] && index < n; index++)
+dest[index] = src[index];
+
+for (index = str_length; index < n; index++)
+dest[index] = '\0';
+return (dest);
+}
