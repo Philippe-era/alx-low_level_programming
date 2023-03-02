75ba2178c38f81d10a7f31346b8005d9abec323b
Philippe-era <jeanphilippe733@gmail.com>
2023-03-02 07:21:20 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,16 +16,16 @@ while (!(str[index] >= 'a' && str[index] <= 'z'))
 if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
-str[index - 1] == '.' ||
 str[index - 1] == ',' ||
 str[index - 1] == ';' ||
+str[index - 1] == '.' ||
 str[index - 1] == '!' ||
 str[index - 1] == '?' ||
 str[index - 1] == '"' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}' ||
 str[index - 1] == '(' ||
 str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
 index == 0)
 str[index] -= 32;
 
