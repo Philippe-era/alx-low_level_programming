b6fb64bd0164ab64af165d29ce5a4684d856645e
Philippe-era <jeanphilippe733@gmail.com>
2023-03-02 06:39:03 -0800

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
 #include "sluggy.h"
 
 /**
- * strcat -  concatenates the string pointed to the src
+ * _strcat -  concatenates the string pointed to the src
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest
  *
  * Return: A pointer to the destination string @dest
  */
 
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
 int i, j;
 
