a63fad8f384008ce1c1a3de8524a5f4d3df32b07
Philippe-era <jeanphilippe733@gmail.com>
2023-03-02 09:04:44 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include "main.h"                                                                                                                    
+#include "sluggy.h"                                                                                                                    
                                                                                                                                      
 /**                                                                                                                                  
  * _strncpy - Copies at most an inputted number                                                                                      
