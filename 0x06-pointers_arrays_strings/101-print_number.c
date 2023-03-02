e3a711e980ed1a32fd6d3bd58b9dd098ca0dc0a1
Philippe-era <jeanphilippe733@gmail.com>
2023-03-02 09:32:41 -0800

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,27 +1,22 @@
-#include "sluggy.h"
-
-/**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
- */
-
-void print_number(int n)
-{
-	unsigned int n1;
-
-	n1 = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n1 = -n;
-	}
-
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+#include "sluggy.h"                                                                                                                     
+                                                                                                                                      
+/**                                                                                                                                   
+ * print_number - Prints an integer.                                                                                                  
+ * @n: The integer to be printed.                                                                                                     
+ */                                                                                                                                   
+void print_number(int n)                                                                                                              
+{                                                                                                                                     
+        unsigned int num = n;                                                                                                         
+                                                                                                                                      
+        if (n < 0)                                                                                                                    
+        {                                                                                                                             
+                _putchar('-');                                                                                                        
+                num = -num;                                                                                                           
+        }                                                                                                                             
+                                                                                                                                      
+        if ((num / 10) > 0)                                                                                                           
+                print_number(num / 10);                                                                                               
+                                                                                                                                      
+        _putchar((num % 10) + '0');                                                                                                   
 }
 
