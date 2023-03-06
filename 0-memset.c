d91cf356da674324c47e4d63f69ac2064db1db90
Philippe-era <jeanphilippe733@gmail.com>
2023-03-05 23:44:59 -0800

    0. memset

---
+++ 0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - Fills the fill n bytes of the memory area
+ * @s: a pointer to the memory filled
+ * @c: The character filled
+ * @n: The number of bytes to be filled
+ * Return: A pointer to the filled memory.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+unsigned int num;
+unsigned char *storage = s, value = c;
+
+for (num = 0; num < n; num++)
+storage[num] = value;
+return (memory);
+}
+
