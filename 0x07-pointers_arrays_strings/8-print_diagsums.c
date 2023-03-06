cf361f821027370ce3ae4159c6867c43187086a3
Philippe-era <jeanphilippe733@gmail.com>
2023-03-06 03:30:12 -0800

    DIAGONAL TRIANGLE

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagadds - Prints the add of the two diagonals
+ * print_diagsums - Prints the add of the two diagonals
  * @a: The integers.
  * @size: the size
  */
-void print_diagadds(int *a, int size)
+void print_diagsums(int *a, int size);
 {
 int num, add1 = 0, add2 = 0;
 for (num = 0; num < size; num++)
