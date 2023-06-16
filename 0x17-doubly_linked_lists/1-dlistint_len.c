4e65a2abc65ed62121b2e7b873a5053cea839e03
= <=>
2023-06-16 04:26:49 -0700

    Task 2

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - the return of the the len
+ * @h: parameter
+ * Return: double linked list to show
+ **/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *node = h;
+	size_t count = 0;
+
+	while (node)
+	{
+		count++;
+		node = node->next_node;
+	}
+	return (count);
+}
