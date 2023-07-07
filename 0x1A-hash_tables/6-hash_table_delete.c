afda9a5b605375c2e7755558cbbc0a0b400a69d3
= <=>
2023-07-07 03:19:28 -0700

    Mandatory completed you know the vibes

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,7 +10,7 @@ void hash_table_delete(hash_table_t *hash_table)
 	hash_node_t *node, *tmp_node;
 	unsigned long int initial;
 
-	for (initial = 0; i < hash_table->size; initial++)
+	for (initial = 0; initial < hash_table->size; initial++)
 	{
 		if (hash_table->array[initial] != NULL)
 		{
